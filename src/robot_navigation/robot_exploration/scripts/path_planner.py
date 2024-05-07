0ad9c77776c4b8b3bcce7f7569aaa2df048593aa
Pablo Ortega <2001.paok@gmail.com>
2024-05-07 16:14:44 -0600

    Modified path planner structure

--- src/robot_navigation/robot_exploration/scripts/path_planner.py
+++ src/robot_navigation/robot_exploration/scripts/path_planner.py
@@ -4,6 +4,7 @@ import rospy
 import numpy as np
 from nav_msgs.msg import OccupancyGrid, Path
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from utils.planners import Planners
 
 class PathPlanner():
     def __init__(self):
@@ -18,12 +19,14 @@ class PathPlanner():
 
         self. goal = None
         self.grid_map = None
+        self.np_grid = None
         self.robot_position = None
         self.path_found = False
         self.ROWS, self.COLS = None, None
 
     def gridmap_callback(self, map: OccupancyGrid):
          self.grid_map = map # (0-100) meaning probability that there is obstacle, -1 if unknown
+         self.np_grid = np.array(self.grid_map.data).reshape(self.grid_map.info.height, self.grid_map.info.width)
          self.ROWS = self.grid_map.info.height
          self.COLS = self.grid_map.info.width
 
@@ -32,6 +35,18 @@ class PathPlanner():
 
     def goal_callback(self, goal):
         self.goal = goal
+
+    def get_cell_from_pose(self, pose_x, pose_y):
+        grid_origin_x, grid_origin_y = (self.grid_map.info.origin.position.x, self.grid_map.info.origin.position.y)
+        grid_resolution = self.grid_map.info.resolution
+        grid_x = int((pose_x - grid_origin_x) / grid_resolution)
+        grid_y = int((pose_y - grid_origin_y) / grid_resolution)
+        return [grid_y, grid_x]
+    
+    def get_pose_from_cell(self, cell):
+        cell_x = cell[1] * self.grid_map.info.resolution + self.grid_map.info.origin.position.x
+        cell_y = cell[0] * self.grid_map.info.resolution + self.grid_map.info.origin.position.y
+        return (cell_x, cell_y)
 
     def get_neighbors(self, cell):
         x, y = cell[0], cell[1]
@@ -47,17 +62,13 @@ class PathPlanner():
     def euclidean_distance(self, point1, point2):
         return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
     
-    def a_star_search(self):
-        rospy.loginfo(f"Searching for path")
-    
     def plan(self):
         while not rospy.is_shutdown():
             if(not self.path_found):
-                self.a_star_search()
+                pass
             else:
                 pass
             self.rate.sleep()
-        raise NotImplemented
 
 if __name__ == "__main__":
     planer = PathPlanner()
---
+++ src/robot_navigation/robot_exploration/scripts/utils/planners.py
@@ -0,0 +1,12 @@
+import math
+import heapq
+
+class Planners():
+    def __init__(self):
+        pass
+
+    def a_star():
+        raise NotImplemented
+    
+    def d_star():
+        raise NotImplemented
